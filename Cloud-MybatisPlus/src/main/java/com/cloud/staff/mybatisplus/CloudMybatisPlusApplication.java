ef7a9732fa947460573a10042a1084bd390bc385
zhaostaffofficer <15196332744@163.com>
2019-01-21 13:29:15 +0800

    MybatisPlus代码生成器

---
+++ Cloud-MybatisPlus/src/main/java/com/cloud/staff/mybatisplus/CloudMybatisPlusApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.staff.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMybatisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMybatisPlusApplication.class, args);
+	}
+
+}
+
---
+++ Cloud-MybatisPlus/src/test/java/com/cloud/staff/mybatisplus/CloudMybatisPlusApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cloud.staff.mybatisplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudMybatisPlusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
