819cc252390188381956c3e0ef580ccb427d2386
zhaostaffofficer <15196332744@163.com>
2019-01-23 22:53:08 +0800

    测试服务

---
+++ Cloud-ApiFirst/src/main/java/com/cloud/staff/apifirst/CloudApiFirstApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.staff.apifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApiFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiFirstApplication.class, args);
+	}
+
+}
+
---
+++ Cloud-ApiSecond/src/main/java/com/cloud/staff/apisecond/CloudApiSecondApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.staff.apisecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApiSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiSecondApplication.class, args);
+	}
+
+}
+
