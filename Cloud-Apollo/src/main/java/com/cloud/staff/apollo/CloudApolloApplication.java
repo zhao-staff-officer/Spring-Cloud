1bea3373986b01d9503c990ceb417877efb99ef1
zhaostaffofficer <15196332744@163.com>
2019-02-14 09:32:16 +0800

    apollo

--- Cloud-Apollo/src/main/java/com/cloud/staff/apollo/CloudApolloApplication.java
+++ Cloud-Apollo/src/main/java/com/cloud/staff/apollo/CloudApolloApplication.java
@@ -2,10 +2,12 @@ package com.cloud.staff.apollo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 
 @EnableApolloConfig
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudApolloApplication {
 
