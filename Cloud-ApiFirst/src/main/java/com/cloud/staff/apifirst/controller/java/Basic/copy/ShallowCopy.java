d856047b0de28f325073120e3aa233778fb0290b
zhaostaffofficer <15196332744@163.com>
2019-04-06 23:45:02 +0800

    Copy

---
+++ Cloud-ApiFirst/src/main/java/com/cloud/staff/apifirst/controller/java/Basic/copy/ShallowCopy.java
@@ -0,0 +1,36 @@
+package com.cloud.staff.apifirst.controller.java.Basic.copy;
+
+/**
+ * 浅拷贝
+ * @author zhaoHB
+ *结果显示
+ *基础类型是指传递
+ *修改一个对象不影响另一个对象
+ */
+public class ShallowCopy {
+	public static void main(String[] args) {
+		int a=1;
+		System.out.println(a);
+		int b=a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(b));
+		b=2;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(b));
+//		1
+//		1
+//		1
+//		2018699554
+//		2018699554
+//		1
+//		2
+//		2018699554
+//		1311053135
+	}
+	
+
+}
