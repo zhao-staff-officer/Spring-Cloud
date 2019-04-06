a18a5e020d6207fe802d92c9d3c07290ce97a386
zhaostaffofficer <15196332744@163.com>
2019-04-06 23:45:25 +0800

    Enum

--- Cloud-ApiFirst/src/main/java/com/cloud/staff/apifirst/controller/java/Basic/Enum/EnumTest.java
+++ Cloud-ApiFirst/src/main/java/com/cloud/staff/apifirst/controller/java/Basic/Enum/EnumTest.java
@@ -5,7 +5,7 @@ public enum EnumTest {
 	Mon("Monday","1"), Tue("Tuesday","3"), Wed("Wednesday","5"), Thu("Thursday","7"), Fri("Friday","2"), Sat("Saturday","8"), 
 	Sun("Sunday","10");
 	 
-	private final String day;
+	private String day;
 	private String num;
 	
 	private EnumTest(String day,String num) {
