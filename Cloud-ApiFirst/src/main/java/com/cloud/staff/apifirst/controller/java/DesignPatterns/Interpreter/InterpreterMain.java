9d26759a1de6c6e0329633e16ebedfe0c2eadc43
zhaostaffofficer <15196332744@163.com>
2019-04-12 23:11:33 +0800

    Lang3DateTime

---
+++ Cloud-ApiFirst/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Interpreter/InterpreterMain.java
@@ -0,0 +1,17 @@
+package com.cloud.staff.apifirst.controller.java.DesignPatterns.Interpreter;
+
+import java.util.StringTokenizer;
+
+/**
+ * 解释器
+ * 为语言创建解释器，通常由语言的语法和语法分析来定义。
+ * @author zhaoHB
+ * TerminalExpression：终结符表达式，每个终结符都需要一个 TerminalExpression。
+ * Context：上下文，包含解释器之外的一些全局信息。
+ */
+public class InterpreterMain {
+	public static void main(String[] args) {
+		
+	}
+
+}
