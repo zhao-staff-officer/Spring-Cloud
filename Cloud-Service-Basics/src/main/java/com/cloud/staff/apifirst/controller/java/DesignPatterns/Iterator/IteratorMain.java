7d6839e8f0a0c0be8be25c38305e3fd60276c56f
zhaostaffofficer <15196332744@163.com>
2019-04-16 23:33:36 +0800

    设计模式-迭代器

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Iterator/IteratorMain.java
@@ -0,0 +1,13 @@
+package com.cloud.staff.apifirst.controller.java.DesignPatterns.Iterator;
+
+/**
+ * 提供一种顺序访问聚合对象元素的方法，并且不暴露聚合对象的内部表示。
+ * @author zhaoHB
+ *
+ */
+public class IteratorMain {
+	public static void main(String[] args) {
+		
+	}
+
+}
