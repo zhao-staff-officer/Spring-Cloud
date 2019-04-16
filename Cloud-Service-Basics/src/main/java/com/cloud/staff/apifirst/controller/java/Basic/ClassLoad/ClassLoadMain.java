197af280b25f29a1b0141b7e8857722065514f2d
zhaostaffofficer <15196332744@163.com>
2019-04-16 23:33:09 +0800

    ClassLoad

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Basic/ClassLoad/ClassLoadMain.java
@@ -0,0 +1,19 @@
+package com.cloud.staff.apifirst.controller.java.Basic.ClassLoad;
+
+/**
+ * class加载器
+ * @author zhaoHB
+ *启动类加载器 bootstrap classload
+ *扩展类加载器 extendtion classload
+ *应用程序加载器 application classload
+ *自定义加载器 User classload
+ *
+ *委派与双亲委派
+ * 类加载器会调用父类加载器，如果父类加载器为空，则调用自身加载器
+ * 双亲委派例如：使用扩展程序加载器 →扩展类加载器→启动类加载器 调用2次
+ * JNDI,JDBC,JCE、JAXB,JBI等
+ */
+public class ClassLoadMain {
+	
+
+}
