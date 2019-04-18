3693d35075375e44ae11cf436fb3ca6fb30931a4
zhaostaffofficer <15196332744@163.com>
2019-04-19 00:38:22 +0800

    设计模式-中介者

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Mediator/MediatorMain.java
@@ -0,0 +1,18 @@
+package com.cloud.staff.apifirst.controller.java.DesignPatterns.Mediator;
+
+/**
+ * Mediator
+ * 中介者
+ * 集中相关对象之间复杂的沟通和控制方式。
+ * Mediator：中介者，定义一个接口用于与各同事（Colleague）对象通信。
+ * Colleague：同事，相关对象
+ * @author zhaoHB
+ *
+ */
+public class MediatorMain {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
