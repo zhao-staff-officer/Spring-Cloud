3a4b11bce6fcd8626e7be21b8379ab2b0b17c36b
zhao-staff-officer <15196332744@163.com>
2019-04-23 23:05:01 +0800

    LocalVariable

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Basic/variable/LocalVariable.java
@@ -0,0 +1,10 @@
+package com.cloud.staff.apifirst.controller.java.Basic.variable;
+
+public class LocalVariable {
+	public static void main(String[] args) {
+		int a,b;
+		a=1;
+		System.out.println(a);
+	}
+
+}
