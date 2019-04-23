d0c2c675c2d94072f19a58d7a32e38b013421bed
zhao-staff-officer <15196332744@163.com>
2019-04-23 21:55:08 +0800

    设计模式-备忘录

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Memento/MementoMain.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Memento/MementoMain.java
@@ -10,5 +10,20 @@ package com.cloud.staff.apifirst.controller.java.DesignPatterns.Memento;
  *         一个是提供给 Originator 的宽接口，允许它访问到先前状态所需的所有数据。理想情况是只允许 Originator 访问本备忘录的内部状态。
  */
 public class MementoMain {
+	
+	public static void main(String[] args) {
+		Originator o = new Originator();
+        Caretaker c = new Caretaker();
+        //改变负责人对象的状态
+        o.setState("On");
+        //创建备忘录对象，并将发起人对象的状态储存起来
+        c.saveMemento(o.createMemento());
+        //修改发起人的状态
+        o.setState("Off");
+        //恢复发起人对象的状态
+        o.restoreMemento(c.retrieveMemento());
+
+        System.out.println(o.getState());
+	}
 
 }
