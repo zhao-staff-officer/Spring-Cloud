1c109387ab4007764b3e9c111cf6c7f3723e7d22
zhao-staff-officer <15196332744@163.com>
2019-04-26 00:09:18 +0800

    设计模式-抽象工厂

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/AbstractFactory/AbstractCreat.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/AbstractFactory/AbstractCreat.java
@@ -2,7 +2,7 @@ package com.cloud.staff.apifirst.controller.java.DesignPatterns.AbstractFactory;
 
 /***
  * 抽象工厂模式创建的是对象家族，也就是很多对象而不是一个对象，并且这些对象是相关的，也就是说必须一起创建出来。
- * @author zhaoHB
+ * @author zhaoHB 
  *
  */
 public abstract class AbstractCreat {
