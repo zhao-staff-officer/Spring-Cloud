d6972eb6f7de5bc1af232ba711ba0241544b0009
zhao-staff-officer <15196332744@163.com>
2019-04-26 00:11:13 +0800

    设计模式-工厂方法

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/FactoryMethod/TestMain.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/FactoryMethod/TestMain.java
@@ -3,7 +3,7 @@ package com.cloud.staff.apifirst.controller.java.DesignPatterns.FactoryMethod;
 /**
  * 工厂方法
  * 是由子类来创建对象
- * 
+ *  
  */
 public class TestMain {
 	public static void main(String[] args) {
