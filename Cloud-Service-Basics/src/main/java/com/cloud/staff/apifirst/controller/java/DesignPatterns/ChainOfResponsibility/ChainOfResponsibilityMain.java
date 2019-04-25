1f7d02d8352bbb14a474491bb2bad887e56da016
zhao-staff-officer <15196332744@163.com>
2019-04-26 00:10:11 +0800

    设计模式-责任链

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/ChainOfResponsibility/ChainOfResponsibilityMain.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/ChainOfResponsibility/ChainOfResponsibilityMain.java
@@ -4,7 +4,7 @@ package com.cloud.staff.apifirst.controller.java.DesignPatterns.ChainOfResponsib
  * 责任链
  * 使多个对象都有机会处理请求，从而避免请求的发送者和接收者之间的耦合关系。
  * 将这些对象连成一条链，并沿着这条链发送该请求，直到有一个对象处理它为止
- * @author zhaoHB
+ * @author zhaoHB 
  *
  */
 public class ChainOfResponsibilityMain {
