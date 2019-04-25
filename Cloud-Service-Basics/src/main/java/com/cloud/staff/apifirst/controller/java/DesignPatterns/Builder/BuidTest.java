a46e55f255f536945156ccae1959a698edf1a40a
zhao-staff-officer <15196332744@163.com>
2019-04-26 00:09:53 +0800

    设计模式-构造器

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Builder/BuidTest.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Builder/BuidTest.java
@@ -7,7 +7,7 @@ package com.cloud.staff.apifirst.controller.java.DesignPatterns.Builder;
  * 将一个对象分解为各个组件
  * 将对象组件的构造封装起来
  * 可以控制整个对象的生成过程
- * @author zhaoHB
+ * @author zhaoHB 
  * 缺点
  *对不同类型的对象需要实现不同的具体构造器的类，这可能回答大大增加类的数量
  */
