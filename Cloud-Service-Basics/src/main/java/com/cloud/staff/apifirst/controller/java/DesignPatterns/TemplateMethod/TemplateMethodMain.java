a12edd38c6108df481c48207979d0cc1eac9289c
zhao-staff-officer <15196332744@163.com>
2019-05-01 23:25:33 +0800

    设计模式-模板

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/TemplateMethod/TemplateMethodMain.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/TemplateMethod/TemplateMethodMain.java
@@ -3,7 +3,8 @@ package com.cloud.staff.apifirst.controller.java.DesignPatterns.TemplateMethod;
 /**
  * 设计模式-模板方法
  * @author zhaoHB
- *
+ *定义算法框架，并将一些步骤的实现延迟到子类。
+ *通过模板方法，子类可以重新定义算法的某些步骤，而不用改变算法的结构。
  */
 public class TemplateMethodMain {
 
