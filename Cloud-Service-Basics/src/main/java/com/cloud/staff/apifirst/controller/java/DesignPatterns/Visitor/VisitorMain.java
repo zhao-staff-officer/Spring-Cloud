0373b3540bb2ea793b35dce1269e332ad1e9d877
zhao-staff-officer <15196332744@163.com>
2019-05-07 01:04:08 +0800

    设计模式-访问者

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Visitor/VisitorMain.java
@@ -0,0 +1,13 @@
+package com.cloud.staff.apifirst.controller.java.DesignPatterns.Visitor;
+
+/**
+ * 设计模式-访问者
+ * @author zhaoHB
+ * 为一个对象结构（比如组合结构）增加新能力。
+ *Visitor：访问者，为每一个 ConcreteElement 声明一个 visit 操作
+ *ConcreteVisitor：具体访问者，存储遍历过程中的累计结果
+ *ObjectStructure：对象结构，可以是组合结构，或者是一个集合。
+ */
+public class VisitorMain {
+
+}
