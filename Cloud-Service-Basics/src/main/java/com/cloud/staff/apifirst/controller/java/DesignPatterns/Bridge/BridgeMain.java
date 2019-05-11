dc7192f381bfa37b089f7af5429b3868e994d0fe
zhao-staff-officer <15196332744@163.com>
2019-05-11 23:35:49 +0800

    设计模式-桥接

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/DesignPatterns/Bridge/BridgeMain.java
@@ -0,0 +1,11 @@
+package com.cloud.staff.apifirst.controller.java.DesignPatterns.Bridge;
+
+/**
+ * 设计模式-桥接
+ * @author 赵参谋
+ * @date 2019-05-11
+ * 将抽象与实现分离开来，使它们可以独立变化。
+ */
+public class BridgeMain {
+
+}
