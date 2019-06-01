2d23ab507811279c8e4fdd81cd0eb6f4aa6e4f3f
zhao-staff-officer <15196332744@163.com>
2019-06-01 15:31:21 +0800

    gitlab
    持续集成

---
+++ Cloud-GitlabCICD/src/main/java/com/cloud/staff/gitlab/CloudGitlabCicdApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.staff.gitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGitlabCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGitlabCicdApplication.class, args);
+	}
+
+}
