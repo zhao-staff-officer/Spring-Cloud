13d3bcfed2e0496d876f2d98595f397511332c57
zhao-staff-officer <15196332744@163.com>
2020-01-17 00:36:37 +0800

    多线程停止interrupt

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/interrupt/InterruptThread.java
@@ -0,0 +1,11 @@
+package com.cloud.staff.apifirst.controller.java.Thread.interrupt;
+
+public class InterruptThread extends Thread{
+    @Override
+    public void run(){
+        super.run();
+        for(int i =0 ;i<500000;i++){
+            System.out.println("i="+(i+1));
+        }
+    }
+}
