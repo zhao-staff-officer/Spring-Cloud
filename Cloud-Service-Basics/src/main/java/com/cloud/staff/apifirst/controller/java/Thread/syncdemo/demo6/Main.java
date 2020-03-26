c4b4e8522259b613f8f0e8672be5eaad490bb0c8
zhao-staff-officer <15196332744@163.com>
2020-03-27 00:06:02 +0800

    重入锁继承性示例

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/syncdemo/demo6/Main.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/syncdemo/demo6/Main.java
@@ -1,4 +1,14 @@
 package com.cloud.staff.apifirst.controller.java.Thread.syncdemo.demo6;
 
 public class Main {
+    public  int i=10;
+    synchronized public void optMainMethod(){
+        try{
+            i--;
+            System.out.println("main print i="+i);
+            Thread.sleep(100);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 }
