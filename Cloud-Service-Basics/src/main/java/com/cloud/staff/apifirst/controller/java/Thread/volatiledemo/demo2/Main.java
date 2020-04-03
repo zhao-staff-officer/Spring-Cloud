2b52d0ef66c486c4de75780e85a1ae11a29659c5
zhao-staff-officer <15196332744@163.com>
2020-04-03 10:32:08 +0800

    volatile特性

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/volatiledemo/demo2/Main.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/volatiledemo/demo2/Main.java
@@ -1,5 +1,11 @@
 package com.cloud.staff.apifirst.controller.java.Thread.volatiledemo.demo2;
 
+/**
+ * volatile
+ * 1：volatile是线程同步轻量级实现，volatile只能修饰变量，synchronized可以修饰变量/方法/代码块。
+ * 2: volatile不会线程阻塞，synchronized会线程阻塞。
+ * 3：volatile解决线程变量可见性，synchronized解决线程资源访问同步性。
+ */
 public class Main {
     public static void main(String[] args) {
         PrintString printString=new PrintString();
