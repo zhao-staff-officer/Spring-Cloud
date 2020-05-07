020038809bd2eb1d864d6ccde4ae9f279fbe0b18
zhao-staff-officer <15196332744@163.com>
2020-05-07 23:37:54 +0800

    新增wait(long)

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/waitnotify/demo5/MainWaitTest.java
@@ -0,0 +1,35 @@
+package com.cloud.staff.apifirst.controller.java.Thread.waitnotify.demo5;
+
+import lombok.SneakyThrows;
+
+/**
+ * @ClassName MainWaitTest
+ * @Description : wait后释放锁
+ * @Return :
+ * @Author : 赵参谋
+ * @Date : 2020/4/28 0:26
+*/
+public class MainWaitTest {
+
+    public static void main(String[] args) {
+        WaitTest waitTest=new WaitTest();
+        Object object=new Object();
+
+        new Thread(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                waitTest.waitTest(object);
+            }
+        },"A").start();
+
+        new Thread(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                waitTest.waitTest(object);
+            }
+        },"B").start();
+
+    }
+}
---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/waitnotify/demo8/SetValue.java
@@ -0,0 +1,7 @@
+package com.cloud.staff.apifirst.controller.java.Thread.waitnotify.demo8;
+
+/**
+ * 生产者
+ */
+public class SetValue {
+}
