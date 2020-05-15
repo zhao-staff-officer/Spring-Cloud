aded353b62922b821c3b95aed3aa7a60d8c5f8fe
zhao-staff-officer <15196332744@163.com>
2020-05-15 22:00:39 +0800

    threadlocal 重写init方法初始化值

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/threadlocal/demo3.java
@@ -0,0 +1,27 @@
+package com.cloud.staff.apifirst.controller.java.Thread.threadlocal;
+
+/**
+ * threadlocal get null 问题
+ */
+public class demo3 {
+
+    static  class ThreadLocalExtends extends ThreadLocal{
+
+        @Override
+        protected Object initialValue() {
+            return "初始化值";
+        }
+
+    public static ThreadLocalExtends threadLocalExtends=new ThreadLocalExtends();
+
+    public static void main(String[] args) {
+        if(threadLocalExtends.get() == null ){
+            System.out.println("未初始化值");
+            threadLocalExtends.set("设置值");
+        }
+        System.out.println(threadLocalExtends.get());
+
+    }
+
+    }
+}
