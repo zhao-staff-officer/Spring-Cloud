38397fa4c6236db8761482791b2bb72117544ecd
zhao-staff-officer <15196332744@163.com>
2020-05-20 10:29:35 +0800

    添加ReentrantLock

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/lock/reentrantlock/demo1.java
@@ -0,0 +1,52 @@
+package com.cloud.staff.apifirst.controller.java.Thread.lock.reentrantlock;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * ReentrantLocak使用
+ */
+public class demo1 {
+
+    static class LockThread {
+        private Lock lock = new ReentrantLock();
+
+        public void testLock() {
+            lock.lock();
+            try {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + "打印：" + i);
+                    Thread.sleep(200);
+                }
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }finally {
+                lock.unlock();
+            }
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        LockThread lockThread=new LockThread();
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               lockThread.testLock();
+           }
+       },"A").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lockThread.testLock();
+            }
+        },"B").start();
+    }
+}
