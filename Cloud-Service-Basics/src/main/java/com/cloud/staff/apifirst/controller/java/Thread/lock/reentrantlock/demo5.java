9e720ce9b050715d74ccd5f6c507df2116ad7458
zhao-staff-officer <15196332744@163.com>
2020-05-26 14:05:40 +0800

    新增reentlock.getholdcount()

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/Thread/lock/reentrantlock/demo5.java
@@ -0,0 +1,36 @@
+package com.cloud.staff.apifirst.controller.java.Thread.lock.reentrantlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * getHoldCount()
+ * 查询当前线程持此锁的个数，也就是调用lock()方法的次数
+ */
+public class demo5 {
+    private ReentrantLock reentrantLock =new ReentrantLock();
+
+    public void serviceMethod1(){
+        reentrantLock.lock();
+        try{
+           System.out.println(Thread.currentThread().getName()+":"+reentrantLock.getHoldCount());
+           serviceMethod2();
+        }finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void serviceMethod2(){
+        reentrantLock.lock();
+        try{
+            System.out.println(Thread.currentThread().getName()+":"+reentrantLock.getHoldCount());
+
+        }finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        demo5 demo5=new demo5();
+        demo5.serviceMethod1();
+    }
+}
