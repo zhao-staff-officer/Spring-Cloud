8ee8fe4c5e21de0ec38060bee93ea9c276e9c29c
zhao-staff-officer <15196332744@163.com>
2020-05-28 14:15:27 +0800

    新增原子字段更新

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/automic/AtomicIntegerFieldUpdaterDemo.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/automic/AtomicIntegerFieldUpdaterDemo.java
@@ -1,7 +1,46 @@
 package com.cloud.staff.apifirst.controller.java.JUC.automic;
 
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
 /**
  * 字段类型更新
+ * atomicIntegerFieldUpdater,atomicLongFieldUpdater
  */
 public class AtomicIntegerFieldUpdaterDemo {
+    // 创建原子更新器，并设置需要更新的对象类和对象的属性
+    private static AtomicIntegerFieldUpdater<User>  atomicUser = AtomicIntegerFieldUpdater.newUpdater(User.class,"age");
+
+    public static void main(String[] args) {
+          User conan = new User("conan",10);
+          atomicUser.getAndIncrement(conan);
+          System.out.println(atomicUser.get(conan));
+    }
+
+
+
+    static class User{
+        private String name;
+        public volatile int age;
+
+        public User(String name,int age){
+            this.name=name;
+            this.age=age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
 }
