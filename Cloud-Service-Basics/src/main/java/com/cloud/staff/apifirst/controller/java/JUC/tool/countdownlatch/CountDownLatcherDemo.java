a8ab2254e9e2c305353a4695824f0866d282a3e3
zhao-staff-officer <15196332744@163.com>
2020-05-28 22:58:15 +0800

    新增CountDownLatcher

---
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/tool/countdownlatch/CountDownLatcherDemo.java
@@ -0,0 +1,25 @@
+package com.cloud.staff.apifirst.controller.java.JUC.tool.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatcherDemo implements Runnable {
+
+    private CountDownLatch countDownLatch;
+
+    public CountDownLatcherDemo(CountDownLatch countDownLatch){
+        this.countDownLatch=countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "正在执行");
+        try {
+            Thread.sleep((long) (Math.random() * 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "执行结束");
+        //数值减1
+        countDownLatch.countDown();
+    }
+}
