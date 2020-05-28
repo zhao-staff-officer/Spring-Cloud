28d167f9cf06c54de371c2d29d8ac0b8809c1fd2
zhao-staff-officer <15196332744@163.com>
2020-05-28 17:01:00 +0800

    新增atomicMarkableReference,atomicStampedReference

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/automic/AtomicReferenceDemo.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/automic/AtomicReferenceDemo.java
@@ -1,10 +1,12 @@
 package com.cloud.staff.apifirst.controller.java.JUC.automic;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * 引用类型
- * atomicReference,atomicReferenceArray,
+ * atomicReference,atomicReferenceArray,atomicReferenceFieldUpdater
  * atomicMarkableReference,atomicStampedReference
  */
 public class AtomicReferenceDemo {
@@ -20,7 +22,29 @@ public class AtomicReferenceDemo {
             atomicReference.compareAndSet(user,updaterUser);
             System.out.println(atomicReference.get().getName());
         }
-
+    }
+    //atomicMarkableReference
+    static  class AtomicMarkableReferenceTest{
+        public static void main(String[] args) {
+            User user1 =new User("zhangsan",10);
+            User user2 =new User("lisi",12);
+            AtomicMarkableReference<User> atomicMarkableReference =new AtomicMarkableReference<>(user1,false);
+            atomicMarkableReference.compareAndSet(user1,user2,false,false);
+            System.out.println(atomicMarkableReference.get(new boolean[1]).getAge());
+        }
+    }
+    //atomicStampedReference
+    static class AtomicStampedReferenceTest{
+        public static void main(String[] args) {
+            User user1 =new User("zhangsan",10);
+            User user2 =new User("lisi",12);
+            AtomicStampedReference<User> atomicStampedReference=new AtomicStampedReference<>(user1,158);
+            int[] stamp = new int[10];
+            System.out.println(atomicStampedReference.get(stamp).getName());
+            System.out.println(atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(user1,user2,stamp[0],2);
+            System.out.println(atomicStampedReference.get(stamp).getName());
+        }
     }
 
 
