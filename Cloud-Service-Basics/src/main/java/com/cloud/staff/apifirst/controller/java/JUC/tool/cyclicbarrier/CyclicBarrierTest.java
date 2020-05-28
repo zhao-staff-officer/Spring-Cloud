989c1dd5ac0b15a732ff7b1928b5e7282483a77c
zhao-staff-officer <15196332744@163.com>
2020-05-29 00:04:49 +0800

    新增CountDownLatcher与CyclicBarrire区别

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/tool/cyclicbarrier/CyclicBarrierTest.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/tool/cyclicbarrier/CyclicBarrierTest.java
@@ -4,6 +4,12 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * CountDownLatcher与CyclicBarrier区别
+ * CoungtDownLatcher是基于AQS实现，CyclicBarrier是基于lock.conndition
+ * CoungtDownLatcher计数器减法，不可以重用。CyclicBarrier基于加法通过reset可以重用
+ * CoungtDownLatcher父线程调用await()等待子线程CoungtDownLatcher计数器为0，CyclicBarrier是子线程嗲用await等待其余线程执行完毕。
+ */
 public class CyclicBarrierTest {
     public static void main(String[] args) {
         //创建线程池
