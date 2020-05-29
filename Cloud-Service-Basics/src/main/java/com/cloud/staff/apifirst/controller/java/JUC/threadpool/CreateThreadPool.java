391a8efa5ab7b268c4ec26889866c513966878d1
zhao-staff-officer <15196332744@163.com>
2020-05-29 14:21:18 +0800

    添加创建线程池执行过程

--- Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/threadpool/CreateThreadPool.java
+++ Cloud-Service-Basics/src/main/java/com/cloud/staff/apifirst/controller/java/JUC/threadpool/CreateThreadPool.java
@@ -31,3 +31,6 @@ public class CreateThreadPool {
 //    BlockingQueue<Runnable> workQueue, 线程等待队列
 //    ThreadFactory threadFactory,  线程创建工厂
 //    RejectedExecutionHandler handler 拒绝策略
+
+//线程池工作过程
+//1：创建核心线程池，2核心线程池满了放入blockingqueue等待队列，3等待队列满了，若果小于最大线程池创建线程执行，反之执行拒绝策略
