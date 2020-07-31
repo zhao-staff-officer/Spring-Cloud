ff286d883ad453c14d3be87ac97cbbddd86a4df8
zhao-staff-officer <15196332744@163.com>
2020-07-31 10:33:13 +0800

    新增netty

---
+++ Cloud-Netty/src/main/java/com/cloud/staff/netty/CloudNettyApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.staff.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNettyApplication.class, args);
+	}
+
+}
+
---
+++ Cloud-Netty/src/test/java/com/cloud/staff/eureka/netty/CloudNettyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cloud.staff.eureka.netty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudNettyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
