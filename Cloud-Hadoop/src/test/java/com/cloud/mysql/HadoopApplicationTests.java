1f8ebd72383d01214f03f830b25efefd6df33fb9
zhao-staff-officer <15196332744@163.com>
2021-12-02 22:18:12 +0800

    新增

---
+++ Cloud-Hadoop/src/test/java/com/cloud/mysql/HadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
